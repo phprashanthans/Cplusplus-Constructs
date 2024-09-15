d79dae95ca66b63f07be9c98b65904dece7c9c9c
Prashant Hans <phprashanthans@gmail.com>
2024-09-15 20:16:00 +0530

    Update Endianness.cpp

--- Endianness.cpp
+++ Endianness.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <bit>
-
+// Since C++20
 int main()
 {
     if constexpr (std::endian::native == std::endian::little) {
@@ -9,6 +9,18 @@ int main()
         std::cout << "This system is big-endian" << std::endl;
     } else {
         std::cout << "This system is mixed-endian" << std::endl;
+    }
+    return 0;
+}
+
+// Before C++20
+int main() {
+    unsigned int num = 0x123456;
+    char* ptr = (char*)&num;
+    if(*ptr == 0x67) {
+        printf("Little Endian\n");
+    } else {
+        printf("Big Endian\n");
     }
     return 0;
 }
