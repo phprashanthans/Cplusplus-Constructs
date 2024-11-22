fc883ae0858f4936ad9a05f15d2e6dffbf02fbe1
Prashant Hans <phprashanthans@gmail.com>
2024-11-22 22:52:14 +0530

    Update SFINAE.cpp

--- SFINAE.cpp
+++ SFINAE.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <type_traits>
 
-/*
+/*----------------------------------------------------------------------------------------------------------------------
 std::enable_if<std::is_arithmetic<T>::value, T>::type: This is a SFINAE (Substitution Failure Is Not An Error) technique. 
 It checks if T is an arithmetic type (like int, float, etc.). If T is arithmetic, the function will be instantiated; 
 otherwise, it will not be available.
 std::is_arithmetic<T>::value evaluates to true for arithmetic types.
 If the condition is true, the type of the function is T, allowing the addition operation.
-*/
+*/----------------------------------------------------------------------------------------------------------------------
 template<typename T>
 typename std::enable_if<std::is_arithmetic<T>::value, T>::type
 MathOperation(T a, T b) {
@@ -20,4 +20,22 @@ int main() {
     auto res = MathOperation(x, y);
     std::cout << "Operation Result:" << res << std::endl;
     return 0;
+}
+
+/*---------------------------------------------------------------------------------/
+SFINAE: Substitution Failure Is Not An Error
+A principle that allows for template specialization based on whether a substitution
+of template parameters is valid or not. It helps in creating more flexible and 
+adaptive template code.
+/---------------------------------------------------------------------------------*/
+
+#include <iostream>
+
+template <typename T>
+auto func(T value) -> decltype(value.someMethod(), void()) {
+    // Implementation for types with 'someMethod'
+}
+template <typename T>
+void func(T value, ...) {
+    // Fallback implementation for other types
 }
