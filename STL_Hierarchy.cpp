d0ce56997b7f4af268741ec91b81019a1c8ff5f4
Prashant Hans <phprashanthans@gmail.com>
2025-03-25 23:00:17 +0530

    Create STL_Hierarchy.cpp

---
+++ STL_Hierarchy.cpp
@@ -0,0 +1,41 @@
+/*----------------------------------------------------------------------------------
+STL (Standard Template Library)
+Primary components of STL - 
+1) Containers: Data Structures to store and organize collections of elements
+    1.1) Sequence Containers:
+        1.1.1) std::vector (Dynamic Array)
+        1.1.2) std::deque (Double Ended Queue)
+        1.1.3) std::list (Doubly Linked List)
+        1.1.4) std::forward_list (Singly Linked List)
+    1.2) Associative Containers: Implemented using Balanced Binary Trees
+        1.2.1) std::set (sorted unique elements)
+        1.2.2) std::map (sorted key-value pairs)
+    1.3) Unordered Containers: Implemented using Hash Tables
+        1.3.1) std::unordered_set (hash based set for unique elements)
+        1.3.2) std::unordered_map (hash based map for key-value pairs)
+    1.4) Container Adapters:
+        1.4.1) std::stack (LIFO)
+        1.4.2) std::queue (FIFO)
+        1.4.3) std::priority_queue (Priority ordered queue)
+2) Algorithms: A set of functions to manipulate data in containers
+    2.1) Non-modifying Algorithms:
+        std::find, std::count, std::accumulate
+    2.2) Modifying Algorithms
+        std::sort, std::reverse, std::transform
+    2.3) Partitioning Algorithms
+        std::partition
+    2.4) Searching and Sorting Algorithms
+        std::binary_search, std::sort, std::merge
+3) Iterators: Abstractions that enable navigation and interaction with container elements
+    3.1) Input Iterators: For reading elements sequentially
+    3.2) Output Iterators: For writing elements sequentially
+    3.3) Forward Iterators: For single direction traversal
+    3.4) Bidirectional Iterators: For both forward and backward traversal
+    3.5) Random Access Iterators: For direct access to elements via indices
+-----------------------------------------------------------------------------------*/
+#include <iostream>
+
+int main() {
+    
+    return 0;
+}
